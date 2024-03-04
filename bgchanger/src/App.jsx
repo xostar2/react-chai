09dc9790aa0b0d258f7a36f8d32fdbcad171860d
xostar2 <rajkishans883@gmail.com>
2024-03-04 11:46:53 +0530

    tailwind

---
+++ bgchanger/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [color, setColor] = useState("olive")
+
+  return (
+      
+      <div className="h-screen flex-wrap justify-center items-center duration-200"
+      style={{backgroundColor: color}}
+      >
+         <div className="fixed  flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+           <div className='flex flex-wrap justify-center gap-3 shadow-lg
+           bg-white px-3 py-2 rounded-r-3xl'>
+            <button
+              className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor:"red"}}
+            >
+
+            </button>
+           </div>
+
+          </div>   
+      </div>
+
+    
+  )
+}
+
+export default App
